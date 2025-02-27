512b4a35863e7c2b9aa560d35d4be6587594b7de
Dias <standlight@ya.ru>
2025-02-27 17:36:08 +0300

    сделал этот сайт

---
+++ fortune-wheel-gates-of-olympus/assets/main.js
@@ -0,0 +1,75 @@
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("DOM полностью загружен и разобран!", new Date().getMilliseconds());
+    
+    setTimeout(() => {
+        document.querySelector('.splashscreen-background').classList.add('hide');
+    }, 1);
+
+    setTimeout(() => {
+        document.querySelector('.splashscreen-background').style.display = "none";
+    }, 500);
+});
+
+
+
+
+let spinNumber = 0;
+let spinning = false;
+document.querySelector('.wheel-button').onclick = function () {
+
+    if (spinning)
+        return;
+
+
+    const wheelList = document.querySelector('.wheel__list');
+    spinNumber++;
+    if (spinNumber == 1) {
+        spinning = true;
+        console.log("spin number one");
+        wheelList.classList.add('wheel__list_animation_type_first-rotation');
+        wheelList.classList.add('wheel__list_first-bonus');
+        document.querySelector('.counter-of-spin__text_lg').innerHTML = '1 TRY'
+
+        setTimeout(() => {
+            spinning = false;
+            wheelList.classList.replace('wheel__list_animation_type_first-default-rotation', 'wheel__list_animation_type_second-default-rotation')
+            wheelList.classList.remove('wheel__list_animation_type_first-rotation');
+
+            document.querySelector('.registration-crutch').insertAdjacentHTML('afterend',
+                `<div data-v-ab9da32d="" class="bonus">
+          <div data-v-ab9da32d="" class="bonus__wrap">
+            <div data-v-ab9da32d="" class="bonus__string"><span data-v-ab9da32d="" class="bonus__sumbol">+</span><span
+                data-v-ab9da32d="" class="bonus__amount">500</span><span data-v-ab9da32d=""
+                class="bonus__sumbol">%</span></div><span data-v-ab9da32d="" class="bonus__dep">TO YOUR DEPOSIT</span>
+          </div>
+        </div>`);
+        }, 4000);
+    }
+
+    if (spinNumber == 2) {
+        console.log("spin number two");
+
+        wheelList.classList.remove('wheel__list_first-bonus');
+        wheelList.classList.add('wheel__list_animation_type_second-rotation');
+        wheelList.classList.add('wheel__list_second-bonus');
+        document.querySelector('.counter-of-spin__text_lg').innerHTML = '0 TRIES'
+
+        setTimeout(() => {
+
+            wheelList.classList.replace('wheel__list_animation_type_second-default-rotation', 'wheel__list_last_position')
+            wheelList.classList.remove('wheel__list_animation_type_second-rotation');
+
+            document.querySelector('.registration-crutch').insertAdjacentHTML('afterend',
+                `<div data-v-dac265dc="" class="bonus__second">
+          <div data-v-dac265dc="" class="bonus__second-wrap"><span data-v-dac265dc=""
+              class="bonus__second-amount">70</span><span data-v-dac265dc="" class="bonus__second-fs">FS</span></div>
+        </div>`);
+        }, 4000);
+
+        setTimeout(() => {
+            document.querySelector('.modal').style.display = "block";
+        }, 7000)
+    }
+};
